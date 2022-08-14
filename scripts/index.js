4719a8381dbf903a49728ee0d42a1fb44d05747f
vineetdhyani <vineetdhyani85@gmail.com>
2022-08-14 14:04:42 +0530

    Checkpoint 11

---
+++ scripts/index.js
@@ -0,0 +1,19 @@
+const ViewMoreBtn=document.querySelector("#viewmore");
+const collapseImgclass=document.querySelectorAll(".collapse");
+
+isclick=true;
+function toggleimages(e){
+    collapseImgclass.forEach(image=>{
+    image.classList.toggle('collapse');
+   
+    })
+    if(isclick){
+        e.target.innerText="View Less"
+        isclick=false;
+       }
+       else{
+        e.target.innerText="View More";
+        isclick=true;
+       }
+
+}
---
+++ scripts/payment.js
@@ -0,0 +1,15 @@
+const PayNowbtn=document.getElementById('paynowbutton');
+const modallogin=document.getElementById('modallogin');
+
+
+    modallogin.addEventListener('click',()=>{
+        console.log("woo");
+        PayNowbtn.removeAttribute('disabled');
+        PayNowbtn.addEventListener('click',()=>{
+            
+            alert("Hi your booking is successfull !!")
+        })
+    })
+    logoutbtn.addEventListener('click',()=>{
+        PayNowbtn.setAttribute('disabled',"");
+    })
