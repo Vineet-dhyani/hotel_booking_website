d70352165b450f4bb7eee40b67367ccb2a5df8ff
vineetdhyani <vineetdhyani85@gmail.com>
2022-08-22 14:20:25 +0530

    Checkpoint 13

--- scripts/detail.js
+++ scripts/detail.js
@@ -1,73 +1,94 @@
+let locationid;
+if (localStorage.getItem("locationid") == null) {
+        locationid = localStorage.getItem('locationidMap');
+} else {
+        locationid = localStorage.getItem('locationid');
+}
 
-let id=localStorage.getItem("id");
 
-console.log(id);
-
-function calculate(e){
+function calculate(e) {
         try {
-           
-                    const adults=document.getElementById('adults');
-                    const fromdate= document.getElementById("fromdate");
-                    const todate=document.getElementById("todate");
-                    const  total=document.getElementById("total");
-                    const  formname=document.getElementById('name');
-                    let date=new Date();
-                    let  fromDate=new Date(fromdate.value);
-                    let toDate=new Date(todate.value);
 
-                    if((fromDate.getTime()-date.getTime())<0)throw Error("from is pastdate");
-                    if((toDate.getTime()-fromDate.getTime())<0) throw Error("todate is pastdate");
-                    
-                    let newdate=((toDate.getTime()-fromDate.getTime())/(86400000));
-                    total.value=newdate*adults.value*1000;  
-                
-                        
+                const adults = document.getElementById('adults');
+                const fromdate = document.getElementById("fromdate");
+                const todate = document.getElementById("todate");
+                const total = document.getElementById("total");
+                const formname = document.getElementById('name');
+                let date = new Date();
+                let fromDate = new Date(fromdate.value);
+                let toDate = new Date(todate.value);
+
+                if ((fromDate.getTime() - date.getTime()) < 0) throw Error("from is pastdate");
+                if ((toDate.getTime() - fromDate.getTime()) < 0) throw Error("todate is pastdate");
+
+                let newdate = ((toDate.getTime() - fromDate.getTime()) / (86400000));
+                total.value = newdate * adults.value * 1000;
+
+
         } catch (error) {
-            console.log(id);
-            total.value=0;
-            console.log(error);  
-       }
- }
- 
 
- 
-function hotelimages(id){
-    
-  
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': '743288e5bdmshd9b12b1b20f3ccfp1607d7jsn9779c46d75d4',
-            'X-RapidAPI-Host': 'hotels4.p.rapidapi.com'
+                total.value = 0;
+                console.log(error);
         }
-    };
-     fetch(`https://hotels4.p.rapidapi.com/properties/get-hotel-photos?id=${id}`, options)
-        .then(response => response.json())
-        .then((response )=>{
-            
-            res=response.hotelImages;
-            for (let index = 0; index <7; index++) {
-                const element = res[index];
-                let link=element.baseUrl;
-                imageslink=link.replace('_{size}','');
-                let images=document.querySelector(".carousel-inner");
-                let str=`<div class="carousel-item active position-static">
-                       <img src="${imageslink}" alt="" width="500" height="350">
-                        </div>
-                        `
-                  console.log(imageslink);
-                images.innerHTML=str;
-              
-                
-            }
-            })
+}
 
-         
-           
-        .catch((err) =>{
 
-            images=document.querySelector(".carousel-inner");
-           str1= `<div class="carousel-item active position-static">
+
+function hoteldetails(id) {
+
+        var today = new Date();
+        var dd = today.getDate();
+        var mm = today.getMonth() + 1;
+        var yyyy = today.getFullYear();
+        var checkin = yyyy + '-' + mm + '-' + dd;
+        const options = {
+                method: 'GET',
+                headers: {
+                        'X-RapidAPI-Key': '743288e5bdmshd9b12b1b20f3ccfp1607d7jsn9779c46d75d4',
+                        'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
+                }
+        };
+        let loader = document.querySelector(".loadcontainer");
+        console.log(loader);
+        loader.style.display = 'flex';
+        fetch(`https://travel-advisor.p.rapidapi.com/hotels/get-details?location_id=${locationid}&checkin=${checkin}&adults=1&lang=en_US&currency=USD&nights=2`, options)
+                .then(response => response.json())
+                .then(response => {
+                        console.log(response);
+                        loader.style.display = 'none';
+                        let hotelname = document.querySelector('#hotelname');
+                        let amenities = document.querySelector('#amenities');
+                        let cityname = document.querySelector('#cityname');
+                        let description = document.querySelector('#descpara');
+                        let address = `${response.data[0].location_string}`;
+                        description.innerHTML = `${response.data[0].description}`
+                        cityname.innerHTML = `${response.data[0].parent_display_name}`
+                        hotelname.innerHTML = `${response.data[0].name}`;
+                        let amen = response.data[0].amenities;
+                        amen.forEach((element) => {
+
+                                let str = `<li>${element.key}</li>`;
+                                amenities.innerHTML += str;
+                        })
+                        let res = response?.data[0]?.photo?.images;
+
+                        let images = document.querySelector(".carousel-inner");
+                        let str = `<div class="carousel-item active position-static">
+                               <img src="${res?.large?.url}" alt="" width="500" height="350">
+                                </div>
+                                `
+
+                        images.innerHTML = str;
+
+
+
+                })
+                .catch(err => {
+                        console.error(err);
+                        loader.style.display = 'none';
+                        let str1;
+                        images = document.querySelector(".carousel-inner");
+                        str1 = `<div class="carousel-item active position-static">
             <img src="https://media-cdn.tripadvisor.com/media/photo-w/17/b3/09/b4/by-the-poolside.jpg" alt="" width="500" height="350">
           </div>
           <div class="carousel-item position-static">
@@ -76,65 +97,19 @@ function hotelimages(id){
           <div class="carousel-item position-static">
             <img src="https://media-cdn.tripadvisor.com/media/photo-w/0e/b2/64/3f/radisson-blu-plaza-delhi.jpg" alt="" width="500" height="350">
           </div>`
-          images.innerHTML+=str1;
-            console.error(err)
-           }
-            );
-       
+                        images.innerHTML += str1;
+
+                        let amenities = document.querySelector('#amenities');
+                        let str = `   <li>Room service</li>
+                                  <li>Restaurat</li>
+                                  <li>Business center</li>
+                                  <li>Fitness center</li>
+                                  <li>Pool</li>`
+                        amenities.innerHTML += str;
+
+
+
+                })
+
 }
- 
-hotelimages(id);
-   
-
-function hoteldetail(id){
-    localStorage.removeItem("id");
-   
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': '743288e5bdmshd9b12b1b20f3ccfp1607d7jsn9779c46d75d4',
-            'X-RapidAPI-Host': 'hotels4.p.rapidapi.com'
-        }
-    };
-    
-    fetch(`https://hotels4.p.rapidapi.com/properties/get-details?id=${id}&checkIn=2020-01-08&checkOut=2020-01-15&adults1=1&currency=USD&locale=en_US`, options)
-        .then(response => response.json())
-        .then((response )=> {
-            let hotelname=document.querySelector('#hotelname');
-            let  amenities=document.querySelector('#amenities');
-            let cityname=document.querySelector('#cityname');
-            let description=document.querySelector('#descpara');
-            let address=`${response.data.body.propertyDescription.address.fullAddress}`;
-            localStorage.setItem('address',address);
-            description.innerHTML=`${response.data.body.atAGlance.keyFacts.specialCheckInInstructions[0]}`
-            cityname.innerHTML=`${response.data.body.propertyDescription.address.provinceName}`
-            hotelname.innerHTML=`${response.data.body.propertyDescription.name}`;
-            localStorage.setItem('hname',`${response.data.body.propertyDescription.name}`);
-            let res=response.data.body.overview.overviewSections[0].content;
-            res.forEach((element,index) => {
-                  console.log(element);
-                let str=`<li>${element}</li>`;
-                amenities.innerHTML+=str;
-                
-            });
-        })
-        .catch((err) => {
-            console.error(err)
-            let  amenities=document.querySelector('#amenities');
-            let str= `   <li>Room service</li>
-                        <li>Restaurat</li>
-                        <li>Business center</li>
-                        <li>Fitness center</li>
-                        <li>Pool</li>`
-            amenities.innerHTML+=str;            
-        });
-}
-        
-hoteldetail(id); 
-
-
-
-
-
-
-
+hoteldetails(locationid);
--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,83 @@
-const ViewMoreBtn=document.querySelector("#viewmore");
-const collapseImgclass=document.querySelectorAll(".collapse");
 
-isclick=true;
-function toggleimages(e){
-    collapseImgclass.forEach(image=>{
-    image.classList.toggle('collapse');
-   
+function clear() {
+    localStorage.removeItem('lat');
+    localStorage.removeItem('lng');
+    localStorage.removeItem('lat1');
+    localStorage.removeItem('lng1');
+    localStorage.removeItem('searchvalue');
+    localStorage.removeItem("inputvalue");
+    localStorage.removeItem("locationid");
+    localStorage.removeItem("locationidMap");
+}
+clear();
+const ViewMoreBtn = document.querySelector("#viewmore");
+const collapseImgclass = document.querySelectorAll(".collapse");
+let city = document.getElementById('city');
+
+isclick = true;
+function toggleimages(e) {
+    collapseImgclass.forEach(image => {
+        image.classList.toggle('collapse');
+
     })
-    if(isclick){
-        e.target.innerText="View Less"
-        isclick=false;
-       }
-       else{
-        e.target.innerText="View More";
-        isclick=true;
-       }
+    if (isclick) {
+        e.target.innerText = "View Less"
+        isclick = false;
+    }
+    else {
+        e.target.innerText = "View More";
+        isclick = true;
+    }
 
 }
 
 
 
-function find(name){
-    localStorage.setItem("searchvalue",name);
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+function initialize() {
+
+    var input = document.getElementById('city');
+
+    var autocomplete = new google.maps.places.Autocomplete(input);
+
+    autocomplete.addListener('place_changed', function () {
+        var place = autocomplete.getPlace();
+
+        let lat = place.geometry['location'].lat();
+
+        let lng = place.geometry['location'].lng();
+        console.log(lat + ":" + lng);
+        localStorage.setItem("lat", lat);
+        localStorage.setItem("lng", lng);
+        let myStr = input.value;
+        let Word = myStr.split(" ")[0]
+        Word = Word.slice(0, -1);
+        localStorage.setItem("inputvalue", Word);
+        window.location.replace(`/list.html?city=${Word}`);
+
+    });
 }
+
+// const input = document.querySelector('#city');
+
+// input.addEventListener("change", updateValue);
+
+
+
+
+
+
+function find(name) {
+    try {
+        localStorage.setItem("searchvalue", name);
+
+        window.location.replace(`/list.html?city=${name}`);
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
